ecd9c93571bdb85d0b1f4aae04fa90a29c84f2c7
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-05-20 09:41:29 +0100

    Added some comments.

--- 3D Cell.cpp
+++ 3D Cell.cpp
@@ -22,22 +22,28 @@ ACell::ACell()
 	PrimaryActorTick.bCanEverTick = true;
 
 	//UStaticMeshComponent* 
+	//This line creates a sub object that is a human visible mesh.
 	MainMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Main"));
 	//MainMesh->SetupAttachment(RootComponent);
 	RootComponent = MainMesh;
-
+	//Each actor needs a root component it's best for this to be the main mesh.
+	
+	//This line finds the mesh that will be used. All the assets are default engine content that the engine won't work without so nothing needs to be downloaded.
 	static ConstructorHelpers::FObjectFinder<UStaticMesh> CubeAsset(TEXT("StaticMesh'/Engine/BasicShapes/Cube1.Cube1'"));
 	
-	if (CubeAsset.Succeeded()) {
+	if (CubeAsset.Succeeded()) {//Checks that the asset has been loadeed successfully. This really shouldn't fail but if it does GOL won't work but it won't crash.
 		MainMesh->SetStaticMesh(CubeAsset.Object);
+		//Sets the mesh of the object.
 	}
 	
+	
+	//Getting and creating materials not important 
 	MainMat = CreateDefaultSubobject<UMaterialInterface>(TEXT("Varient Material"));
 	OtherMat = CreateDefaultSubobject<UMaterialInterface>(TEXT("Other Material"));
 	static ConstructorHelpers::FObjectFinder<UMaterial> mDeadAsset(TEXT("Material'/Engine/VREditor/UI/ArrowMaterial.ArrowMaterial'")); //These calls are going to need to succeed --Translucent blue
 	static ConstructorHelpers::FObjectFinder<UMaterial> mAliveAsset(TEXT("Material'/Engine/BasicShapes/BasicShapeMaterial.BasicShapeMaterial'")); //opaque white
 
-
+	//Setting up of the collision box.
 	CollBox = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
 	//CollBox->SetWorldScale3D(FVector(1.2f,1.2f,1.2f));
 	CollBox->SetBoxExtent(FVector(65,65,65));
@@ -80,6 +86,7 @@ ACell::ACell()
 void ACell::BeginPlay() //This function is initiated upon play. All it does it wait for 1 second the call another function.
 {
 	Super::BeginPlay();
+	//When the game starts a loop is created that waits then runs a function. I don't think there's another way to do it.
 	GetWorld()->GetTimerManager().SetTimer(MainLoop, this, &ACell::InitCell, 1, true, 1.0f);
 
 }
@@ -91,9 +98,11 @@ void ACell::Tick(float DeltaTime) // This function is somewhat unneeded however 
 	Super::Tick(DeltaTime);
 	if (bAlive) {
 		MainMesh->SetVisibility(true,false);
+		//^Makes the cube visible
 	}
 	else {
 		MainMesh->SetVisibility(false, false);
+		//^Makes the cube invisible just ignore the second bool
 	}
 	//onTimerFire();
 
@@ -101,15 +110,19 @@ void ACell::Tick(float DeltaTime) // This function is somewhat unneeded however 
 
 void ACell::InitCell() //This inititates the cell, setting up the randomness and then calling the main looping function.
 {
+	//This thing is only ran once. The reason it needs to wait before it is run is because other wise not all of the cubes would have spawned.
 	bAlive = rand() % 2;
 	//TArray<AActor*> NeighboursInit;
-	GetOverlappingActors(NeighboursInit, TSubclassOf<ACell>());
+	GetOverlappingActors(NeighboursInit, TSubclassOf<ACell>()); //Sets a variable to all the objects its overlapping.
 	//FTimerHandle MainLoop;
 	if (GetActorLocation() == FVector(0, 0, 0))
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("How many neighbours x: %f"), NeighboursInit.Num()));
 	}
+	//^This code is more for debuging the if statement checks to make sure that it's only the cell at 0,0,0 that runs the code so that only one print statement will occure.
+	//Note the command AddOnScreenDebugMessage will only be used in editor versions hence it's fine if an exe is needed.
 	GetWorld()->GetTimerManager().SetTimer(MainLoop, this, &ACell::onTimerFire, 1, true, 0.5f);
+	//^The main loop
 }
 
 void ACell::onTimerFire() //every 0.5 seconds this function occurs. It makes sure that all the neighbours work properly and then it will increment the neighbour number if that is true.
