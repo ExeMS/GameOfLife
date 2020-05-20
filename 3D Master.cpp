c6c19b946120b4bd40be72f342906bd2d897bd82
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-05-20 09:45:17 +0100

    Added some comments

--- 3D Master.cpp
+++ 3D Master.cpp
@@ -3,6 +3,8 @@
 #include "Engine/World.h"
 
 // Sets default values
+//This class only exists to spawn all the cells.
+//Pretty sure I can use something like this in my EMC for world loading if I use a construction script
 AMaster::AMaster()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
@@ -17,6 +19,7 @@ void AMaster::BeginPlay()
 {
 	Super::BeginPlay();
 	FActorSpawnParameters SpawnParams;
+	//^nothing important needed here in this case.
 	for (size_t x = 0; x < 10; x++)
 	{
 		for (size_t y = 0; y < 10; y++)
@@ -24,8 +27,9 @@ void AMaster::BeginPlay()
 			for (size_t z = 0; z < 10; z++)
 			{
 				GetWorld()->SpawnActor<AActor>(ActorToSpawn, FVector(100*x,100*y,100*z), FRotator(0,0,0),SpawnParams);
-
-			}
+				//^ spawns an actor of the type that is specifed in a variable exposed per instance. Sets the position and rotation.
+				//A great example of object orientation which in this case is Object(epic games') -> Actor(epic games') -> Master(mine)
+			}	
 
 		}
 	}
