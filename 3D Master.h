04cf193068c19263052fc0370497c277843750a1
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-05-21 23:07:35 +0100

    Added part of a start ui element.

--- 3D Master.h
+++ 3D Master.h
@@ -12,6 +12,10 @@ class EMCGAME_API AMaster : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AMaster();
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<AActor> ActorToSpawn;
+	UPROPERTY(EditAnywhere) //The particular widget class for an object to be spawned from.
+		TSubclassOf<UUserWidget> WidgetClassVar;
 
 protected:
 	// Called when the game starts or when spawned
@@ -20,8 +24,7 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	UPROPERTY(EditAnywhere)
-		TSubclassOf<AActor> ActorToSpawn;
+
 	// ^ This code isn't really needed however it does allow the user to interchange the cell that is spawned.
 
 };
