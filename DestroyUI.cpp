321b22b7f7364cbabe8429482b4aa580c08e1110
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-05-22 15:04:48 +0100

    Update DestroyUI.cpp

--- DestroyUI.cpp
+++ DestroyUI.cpp
@@ -1,7 +1,12 @@
 #include "DestroyUI.h"
-
-void UDestroyUI::DelayedDestroySelf(UUserWidget * Caller) //Yes this does really on a blueprint but for UI elements those are necessary.
+//Yes this does really on a blueprint but for UI elements those are necessary.
+void UDestroyUI::DelayedDestroySelf(UUserWidget * Caller) 
+	// Takes in a ui element called caller which the blueprints(a node based language) feed a value into.
+	//It the destroys this widget.
 {
-	Caller->RemoveFromParent(); //Remove from the parent widget
-	Caller->Destruct(); //Destroy it, I think this deals with memory leaks.
+	Caller->RemoveFromParent(); 
+	//Remove from the parent widget
+	
+	Caller->Destruct(); 
+	//Destroy it, I think this deals with memory leaks.
 } 
